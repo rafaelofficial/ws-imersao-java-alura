40a08027de6442b6fcc430a28886cdc31e2fc673
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-07-19 18:08:46 -0300

    Custom Terminal output

--- README.md
+++ README.md
@@ -8,4 +8,18 @@
  - S.O.L.I.D 
  - etc.
 
-## Class 01: Consuming an API of movies with Java;
+## Classroom 01: Consuming an API of movies with Java:
+### Challengers this classroom:
+
+ - Consume the endpoint of must popular movies from IMDB API. Find as well, 
+at documentation from IMDB API, the endpoint that return the best series and the
+then return the most popular series.
+ - Use your creativity to give the output data most cute: use emojis with UTF-8 Code, 
+show the note of movies with starts, decor the terminal with colors, bold and italic
+using ANSI codes, and more.
+ - Put the key IMDB API in anywhere out of code like a configuration file (p.ex, a file .properties)
+or environment variable.
+ - Change the class JsonParse for use a parsing library from JSON with Jackson or GSON.
+ - #### Supreme challenger: 
+ Create any ways for give a rating at movie, pulling of any configuration file or
+ asking for user enter at terminal.
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -9,8 +10,8 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        // to do a connection HTTP and get data the top 250 movies from Imdb:
-        String url = "https://imdb-api.com/en/API/Top250TVs/k_p037jo73";
+        // to do a connection HTTP and get data the top 250 movies from Imdb: https://imdb-api.com/en/API/Top250TVs/k_p037jo73
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
         URI address = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(address).GET().build();
@@ -23,9 +24,9 @@ public class App {
         
         // show and manipulated the data;
         for (Map<String, String> movie : listOfMovies) {
-            System.out.println(movie.get("title"));
-            System.out.println(movie.get("image"));
-            System.out.println(movie.get("imDbRating"));
+            System.out.println("\u001b[33mTitle:\033[m " + movie.get("title"));
+            System.out.println("\u001b[33mPoster:\033[m  " + movie.get("image"));
+            System.out.println("\u001b[30;1m\u001b[43;1m Rating: " + movie.get("imDbRating") + " \u001b[m");
             System.out.println();
         }
     }
